ddc433d0826f57d0e413e0356ce31170efc289e6
Ariel Vola <avola@cdainfo.com>
2020-08-01 15:38:22 -0300

    Reconocimiento en tiempo real con la webCam

--- main.js
+++ main.js
@@ -15,6 +15,7 @@ Promise.all( [
     faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
     faceapi.nets.faceExpressionNet.loadFromUri('/models'),
     faceapi.nets.ageGenderNet.loadFromUri('/models'), 
+    faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
 ]).then(startVideo)
 
 video.addEventListener('play', () => {
@@ -24,13 +25,39 @@ video.addEventListener('play', () => {
 
     faceapi.matchDimensions(canvas, displaySize);
     setInterval(async () => {
-        const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
-            const resizedDetections = faceapi.resizeResults(detections,displaySize);
 
-        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
 
-            faceapi.draw.drawDetections(canvas, resizedDetections);
-            faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
-            faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+        const labeledFaceDescriptors = await loadLabeledImages()
+        const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6)
+
+        const detections = await faceapi.detectAllFaces(video).withFaceLandmarks().withFaceDescriptors();
+
+        const resizedDetections = faceapi.resizeResults(detections, displaySize);
+
+        const results = resizedDetections.map(d => faceMatcher.findBestMatch(d.descriptor))
+
+        console.log(results.toString());
+
+        results.forEach((result, i) => {
+            const box = resizedDetections[i].detection.box
+            const drawBox = new faceapi.draw.DrawBox(box, { label: result.toString() })
+            drawBox.draw(canvas)
+          })
     }, 100);
 });
+
+function loadLabeledImages() {
+    const labels = ['Capitan America', 'Chino', 'Thor', 'Aylin', 'Ariel Vola', 'Joao Paura']
+  return Promise.all(
+    labels.map(async label => {
+      const descriptions = []
+      for (let i = 1; i <= 2; i++) {
+        const img = await faceapi.fetchImage(`https://raw.githubusercontent.com/ArielVola/Reconocimineto-Rostros-Js-v2/master/labeled_images/${label}/${i}.jpg`)
+        const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
+        descriptions.push(detections.descriptor)
+      }
+
+      return new faceapi.LabeledFaceDescriptors(label, descriptions)
+    })
+  )
+}
